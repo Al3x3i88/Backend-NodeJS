74bc1d445dcfd2a876891d733c1e78ce2dbbb99a
Alexei Ramirez Duran <alexeirmrz3@gmail.com>
2023-01-27 11:59:37 -0500

    Leyendo cabeceras de las peticiones

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyparser = require('body-parser')
 const router = express.Router();
+const PORT = process.env.PORT || 3001;
 
 var app = express();
 app.use(bodyparser.urlencoded({extended : false }))
 app.use(router);
 
 router.get('/message', function (req, res){
+    console.log(req.headers);
+    res.header({
+        "custom-header": "Nuestro valor personalizado"
+    })
     res.send('Lista de Mensajes')
 })
 
@@ -16,5 +21,7 @@ router.delete('/message', function (req, res){
     res.send('Mensaje   '+ req.body.text + ' Anadido correctamente')
 })
 
-app.listen(3000)
-console.log(`Server on port ${3000}`);
+app.listen(PORT, ()=>{
+
+    console.log(`Server on port ${PORT}`);
+})
